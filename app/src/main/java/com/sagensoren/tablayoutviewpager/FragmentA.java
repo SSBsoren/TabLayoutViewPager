4c7759d0281b87cf190ce8148f62b23027eebe64
SAGEN SAKAM BHIM SOREN <35399866+SSBsoren@users.noreply.github.com>
2019-03-09 14:05:29 +0530

    Initial commit

---
+++ app/src/main/java/com/sagensoren/tablayoutviewpager/FragmentA.java
@@ -0,0 +1,4 @@
+package com.sagensoren.tablayoutviewpager;
+
+public class FragmentA {
+}
---
+++ app/src/main/java/com/sagensoren/tablayoutviewpager/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sagensoren.tablayoutviewpager;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
